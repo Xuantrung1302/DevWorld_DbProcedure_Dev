1d3abb9e9878fa9de92b1d9391753cd5c80aebc1
Huymess11 <huymess0610@gmail.com>
2025-07-07 23:51:25 +0700

    update Generate class schedule

---
+++ SP_GENERATE_CLASS_SCHEDULE.sql
@@ -0,0 +1,65 @@
+USE [DEV_ACADEMY]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER PROCEDURE [dbo].[SP_GENERATE_CLASS_SCHEDULE]
+    @ClassID UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @ClassName VARCHAR(50),
+            @DaysOfWeek VARCHAR(20),
+            @StartTime DATETIME,
+            @EndTime DATETIME,
+            @SemesterID VARCHAR(10),
+            @SemesterStartDate DATETIME,
+            @SemesterEndDate DATETIME;
+
+    SELECT 
+        @ClassName = C.ClassName,
+        @DaysOfWeek = C.DaysOfWeek,
+        @StartTime = C.StartTime,
+        @EndTime = C.EndTime,
+        @SemesterID = S.SemesterID
+    FROM CLASS C
+    INNER JOIN SUBJECT SJ ON C.SubjectID = SJ.SubjectID
+    INNER JOIN SEMESTER S ON SJ.SemesterID = S.SemesterID
+    WHERE C.ClassID = @ClassID;
+
+    SELECT 
+        @SemesterStartDate = StartDate,
+        @SemesterEndDate = EndDate
+    FROM SEMESTER
+    WHERE SemesterID = @SemesterID;
+
+    DECLARE @CurrentDate DATE = @SemesterStartDate;
+    DECLARE @WeekDayNum VARCHAR(2);
+
+    WHILE @CurrentDate <= @SemesterEndDate
+    BEGIN
+        SET @WeekDayNum = CAST(DATEPART(WEEKDAY, @CurrentDate) AS VARCHAR);
+
+        IF CHARINDEX(@WeekDayNum, @DaysOfWeek) > 0
+        BEGIN
+            INSERT INTO CLASS_SCHEDULE (Class_ScheID, ClassID, ClassName, DayOfWeek, StartTime, EndTime)
+            VALUES 
+            (
+                NEWID(), 
+                @ClassID, 
+                @ClassName, 
+                @WeekDayNum, 
+                DATEADD(HOUR, DATEPART(HOUR, @StartTime), CAST(@CurrentDate AS DATETIME)),
+                DATEADD(HOUR, DATEPART(HOUR, @EndTime), CAST(@CurrentDate AS DATETIME))
+            );
+        END
+
+        SET @CurrentDate = DATEADD(DAY, 1, @CurrentDate);
+    END
+END
+GO
+
