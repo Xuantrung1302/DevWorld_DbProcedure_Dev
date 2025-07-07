b69302aa51615970a425b71a43e121250d6da359
Huymess11 <huymess0610@gmail.com>
2025-07-08 00:03:15 +0700

    commit update generate class schedule

--- SP_GENERATE_CLASS_SCHEDULE.sql
+++ SP_GENERATE_CLASS_SCHEDULE.sql
@@ -40,11 +40,32 @@ BEGIN
     DECLARE @CurrentDate DATE = @SemesterStartDate;
     DECLARE @WeekDayNum VARCHAR(2);
 
+    DECLARE @DayCount TABLE
+    (
+        DayNum VARCHAR(2) PRIMARY KEY,
+        Count INT
+    );
+
+    DECLARE @pos INT = 0, @nextPos INT, @len INT;
+    DECLARE @day VARCHAR(2);
+    SET @DaysOfWeek = @DaysOfWeek + ',';
+    SET @len = LEN(@DaysOfWeek);
+
+    WHILE @pos < @len
+    BEGIN
+        SET @nextPos = CHARINDEX(',', @DaysOfWeek, @pos + 1);
+        SET @day = SUBSTRING(@DaysOfWeek, @pos + 1, @nextPos - @pos - 1);
+
+        INSERT INTO @DayCount(DayNum, Count) VALUES (@day, 0);
+
+        SET @pos = @nextPos;
+    END
+
     WHILE @CurrentDate <= @SemesterEndDate
     BEGIN
         SET @WeekDayNum = CAST(DATEPART(WEEKDAY, @CurrentDate) AS VARCHAR);
 
-        IF CHARINDEX(@WeekDayNum, @DaysOfWeek) > 0
+        IF EXISTS (SELECT 1 FROM @DayCount WHERE DayNum = @WeekDayNum AND Count < 8)
         BEGIN
             INSERT INTO CLASS_SCHEDULE (Class_ScheID, ClassID, ClassName, DayOfWeek, StartTime, EndTime)
             VALUES 
@@ -56,10 +77,13 @@ BEGIN
                 DATEADD(HOUR, DATEPART(HOUR, @StartTime), CAST(@CurrentDate AS DATETIME)),
                 DATEADD(HOUR, DATEPART(HOUR, @EndTime), CAST(@CurrentDate AS DATETIME))
             );
+
+            UPDATE @DayCount
+            SET Count = Count + 1
+            WHERE DayNum = @WeekDayNum;
         END
 
         SET @CurrentDate = DATEADD(DAY, 1, @CurrentDate);
     END
 END
 GO
-
