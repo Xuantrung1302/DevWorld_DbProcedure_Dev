738ebb674365b0cb53ed5ec687e8160e4f2fe6ff
Huymess11 <huymess0610@gmail.com>
2025-07-16 22:30:30 +0700

    update Class

--- SP_SELECT_CLASS.sql
+++ SP_SELECT_CLASS.sql
@@ -1,12 +1,9 @@
 ﻿ALTER PROCEDURE [dbo].[SP_SELECT_CLASS]
-    @ClassID UNIQUEIDENTIFIER = NULL,
-    @SubjectID VARCHAR(10) = NULL,
-	@SemesterID VARCHAR(10) = NULL
+    @CourseID UNIQUEIDENTIFIER = NULL,
+    @ClassName VARCHAR(50) = NULL
 AS
 BEGIN
-    --SET NOCOUNT ON;
-
-    SELECT 
+    SELECT
         C.ClassID,
         C.ClassName,
         C.StartTime,
@@ -14,17 +11,14 @@ BEGIN
         C.Room,
         C.MaxSeats,
         C.DaysOfWeek,
-		C.StudentCount,
-        S.SubjectName,
-        Sem.SemesterName,
-        T.FullName AS TeacherName
+        C.StudentCount,
+        T.FullName AS TeacherName,
+        Co.course_name
     FROM CLASS C
-    INNER JOIN SUBJECT S ON C.SubjectID = S.SubjectID
-    INNER JOIN SEMESTER Sem ON S.SemesterID = Sem.SemesterID
-    LEFT JOIN TEACHER T ON C.TeacherID = T.TeacherID  -- Đổi INNER thành LEFT
-    WHERE 
-        (@ClassID IS NULL OR C.ClassID = @ClassID)
-        AND (@SubjectID IS NULL OR C.SubjectID = @SubjectID)
-		AND (@SemesterID IS NULL OR Sem.SemesterID = @SemesterID)
+    INNER JOIN Course Co ON C.course_id = Co.course_id
+    LEFT JOIN TEACHER T ON C.TeacherID = T.TeacherID
+    WHERE
+        (@CourseID IS NULL OR C.course_id = @CourseID)
+        AND (@ClassName IS NULL OR C.ClassName LIKE '%' + @ClassName + '%')
     ORDER BY C.StartTime;
-END
+END;
