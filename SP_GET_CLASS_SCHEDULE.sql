f49e58eed577921c504a787a0dcbe2ca8d6ea7f1
Huymess11 <huymess0610@gmail.com>
2025-07-18 01:53:42 +0700

    update

--- SP_GET_CLASS_SCHEDULE.sql
+++ SP_GET_CLASS_SCHEDULE.sql
@@ -2,46 +2,69 @@ USE [DEV_ACADEMY]
 GO
 ALTER PROCEDURE [SP_GET_CLASS_SCHEDULE]
     @CourseID UNIQUEIDENTIFIER,
-    @ClassID UNIQUEIDENTIFIER
-    --@SemesterID VARCHAR(10)
+    @ClassID UNIQUEIDENTIFIER,
+    @SubjectID VARCHAR(10) = NULL
 AS
 BEGIN
     SET NOCOUNT ON;
 
-    SELECT 
-        cs.Class_ScheID,
-        cs.ClassID,
-        cs.ClassName,
-        cs.SubjectID,
-        sj.SubjectName,
-        cs.DayOfWeek,
-        cs.StartTime,
-        cs.EndTime,
-        sm.SemesterID,
-        sm.SemesterName,
-        cr.course_id AS CourseID,
-        cr.course_code,
-        cr.course_name,
-		T.FullName
-    FROM CLASS_SCHEDULE cs
-    INNER JOIN CLASS c ON cs.ClassID = c.ClassID
-    INNER JOIN SUBJECT sj ON cs.SubjectID = sj.SubjectID
-    INNER JOIN SEMESTER sm ON sj.SemesterID = sm.SemesterID
-    INNER JOIN Course cr ON sm.course_id = cr.course_id
-	INNER JOIN TEACHER T ON T.TeacherID = C.TeacherID
-    WHERE
-        cr.course_id = @CourseID
-        AND cs.ClassID = @ClassID
-        --AND sm.SemesterID = @SemesterID
-    ORDER BY cs.StartTime;
+    IF @SubjectID IS NOT NULL
+    BEGIN
+        SELECT TOP 1
+            cs.Class_ScheID,
+            cs.ClassID,
+            cs.ClassName,
+            cs.SubjectID,
+            sj.SubjectName,
+            cs.DayOfWeek,
+            cs.StartTime,
+            cs.EndTime,
+            sm.SemesterID,
+            sm.SemesterName,
+            cr.course_id AS CourseID,
+            cr.course_code,
+            cr.course_name,
+            T.FullName
+        FROM CLASS_SCHEDULE cs
+        INNER JOIN CLASS c ON cs.ClassID = c.ClassID
+        INNER JOIN SUBJECT sj ON cs.SubjectID = sj.SubjectID
+        INNER JOIN SEMESTER sm ON sj.SemesterID = sm.SemesterID
+        INNER JOIN Course cr ON sm.course_id = cr.course_id
+        INNER JOIN TEACHER T ON T.TeacherID = C.TeacherID
+        WHERE
+            cr.course_id = @CourseID
+            AND cs.ClassID = @ClassID
+            AND cs.SubjectID = @SubjectID
+        ORDER BY cs.StartTime DESC
+    END
+    ELSE
+    BEGIN
+        SELECT 
+            cs.Class_ScheID,
+            cs.ClassID,
+            cs.ClassName,
+            cs.SubjectID,
+            sj.SubjectName,
+            cs.DayOfWeek,
+            cs.StartTime,
+            cs.EndTime,
+            sm.SemesterID,
+            sm.SemesterName,
+            cr.course_id AS CourseID,
+            cr.course_code,
+            cr.course_name,
+            T.FullName
+        FROM CLASS_SCHEDULE cs
+        INNER JOIN CLASS c ON cs.ClassID = c.ClassID
+        INNER JOIN SUBJECT sj ON cs.SubjectID = sj.SubjectID
+        INNER JOIN SEMESTER sm ON sj.SemesterID = sm.SemesterID
+        INNER JOIN Course cr ON sm.course_id = cr.course_id
+        INNER JOIN TEACHER T ON T.TeacherID = C.TeacherID
+        WHERE
+            cr.course_id = @CourseID
+            AND cs.ClassID = @ClassID
+        ORDER BY cs.StartTime
+    END
 END
 GO
---select * from CLASS_SCHEDULE
 
---exec SP_GET_CLASS_SCHEDULE 'A87B8BEC-C779-45E7-ADC6-B61F42294939', 'BEF65320-EF19-4386-96F2-E16BD572A946', 'HK00000001'
-
---select * from Course
---go
---select * from CLASS
---go
---select * from SEMESTER
--- SP_SELECT_SUBJECT.sql
+++ SP_SELECT_SUBJECT.sql
@@ -7,21 +7,23 @@ GO
 
 ALTER PROCEDURE [dbo].[SP_SELECT_SUBJECT]
     @SubjectID VARCHAR(10) = NULL,
-    @SemesterID VARCHAR(10) = NULL
+    @SemesterID VARCHAR(10) = NULL,
+    @ClassID UNIQUEIDENTIFIER = NULL
 AS
 BEGIN
     SET NOCOUNT ON;
 
-    SELECT 
+    SELECT DISTINCT
         S.SubjectID,
         S.SubjectName,
         SM.SemesterName,
         S.TuitionFee
-        --S.DELETE_FLG
     FROM SUBJECT S
     INNER JOIN SEMESTER SM ON S.SemesterID = SM.SemesterID
+    LEFT JOIN CLASS_SCHEDULE CS ON S.SubjectID = CS.SubjectID
     WHERE (S.DELETE_FLG IS NULL OR S.DELETE_FLG = 0)
+      AND (SM.DELETE_FLG IS NULL OR SM.DELETE_FLG = 0)
       AND (@SubjectID IS NULL OR S.SubjectID LIKE '%' + @SubjectID + '%')
-      AND (@SemesterID IS NULL OR S.SemesterID = @SemesterID)
-      AND (SM.DELETE_FLG IS NULL OR SM.DELETE_FLG = 0);
+      AND (@SemesterID IS NULL OR SM.SemesterID = @SemesterID)
+      AND (@ClassID IS NULL OR CS.ClassID = @ClassID);
 END
