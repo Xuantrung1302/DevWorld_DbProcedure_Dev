f7c51e5684ce0cf534072962eb504676f770ef9a
Huymess11 <huymess0610@gmail.com>
2025-07-23 01:14:10 +0700

    update Insert Exam Result

--- SP_INSERT_EXAM_RESULT.sql
+++ SP_INSERT_EXAM_RESULT.sql
@@ -1,23 +1,35 @@
-USE [DEV_ACADEMY]
+﻿USE [DEV_ACADEMY]
 GO
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE PROCEDURE [dbo].[SP_INSERT_EXAM_RESULT]
-    @ExamID UNIQUEIDENTIFIER,
-    @StudentID VARCHAR(10),
-    @Score DECIMAL(5,2),
-    @Status NVARCHAR(20),
-    @EnteredBy VARCHAR(10),
-    @GradingDate DATETIME
+ALTER PROCEDURE [dbo].[SP_INSERT_EXAM_RESULT]
+    @json NVARCHAR(MAX)
 AS
 BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @ExamID UNIQUEIDENTIFIER,
+            @StudentID VARCHAR(10),
+            @Score DECIMAL(5,2),
+            @Status NVARCHAR(20),
+            @EnteredBy VARCHAR(10),
+            @GradingDate DATETIME;
+
+
+    SELECT
+        @ExamID = JSON_VALUE(@json, '$.ExamID'),
+        @StudentID = JSON_VALUE(@json, '$.StudentID'),
+        @Score = TRY_CAST(JSON_VALUE(@json, '$.Score') AS DECIMAL(5,2)),
+        @Status = JSON_VALUE(@json, '$.Status'),
+        @EnteredBy = JSON_VALUE(@json, '$.EnteredBy'),
+        @GradingDate = TRY_CAST(JSON_VALUE(@json, '$.GradingDate') AS DATETIME);
 
     INSERT INTO EXAM_RESULT 
     (ResultID, ExamID, StudentID, Score, Status, EnteredBy, GradingDate, DELETE_FLG)
     VALUES 
     (NEWID(), @ExamID, @StudentID, @Score, @Status, @EnteredBy, @GradingDate, 0);
 END
-GO
+
