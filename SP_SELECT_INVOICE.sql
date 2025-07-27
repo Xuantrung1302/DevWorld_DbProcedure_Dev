be555ac0eb36e132607fc3e4fa72e5e8e1310fe4
Huymess11 <huymess0610@gmail.com>
2025-07-27 21:29:57 +0700

    update

--- SP_SELECT_INVOICE.sql
+++ SP_SELECT_INVOICE.sql
@@ -6,9 +6,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 ALTER PROCEDURE [dbo].[SP_SELECT_INVOICE]
-    @StudentID VARCHAR(10) = NULL,
-    @SemesterID VARCHAR(10) = NULL,
-    @Status NVARCHAR(20) = NULL
+    @StudentID VARCHAR(10) = NULL
 AS
 BEGIN
     SET NOCOUNT ON;
@@ -16,21 +14,18 @@ BEGIN
     SELECT 
         I.InvoiceID,
         I.StudentID,
-        I.SemesterID,
-        C.course_id,             
-        C.course_name,           
+        CO.course_id,
+        CO.course_name,
         I.InvoiceDate,
         I.DueDate,
         I.Amount,
         I.Status
     FROM INVOICE I
-    LEFT JOIN SEMESTER S ON I.SemesterID = S.SemesterID
-    LEFT JOIN COURSE C ON S.course_id = C.course_id
+    LEFT JOIN COURSE CO ON I.course_id = CO.course_id
     WHERE
         (@StudentID IS NULL OR I.StudentID = @StudentID)
-        AND (@SemesterID IS NULL OR I.SemesterID = @SemesterID)
-        AND (@Status IS NULL OR I.Status = @Status)
         AND (I.DELETE_FLG IS NULL OR I.DELETE_FLG = 0)
 END
 GO
+
 
