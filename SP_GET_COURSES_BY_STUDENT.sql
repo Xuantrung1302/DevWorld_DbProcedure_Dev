68e42e209e0a808e9e49f2601e4fcb41160288b4
Dinh Manh Tri <dinhmanhtri002@gmail.com>
2025-07-30 22:42:13 +0700

    Lay khoa hoc theo hoc sinh

--- SP_GET_COURSES_BY_STUDENT.sql
+++ SP_GET_COURSES_BY_STUDENT.sql
@@ -1,27 +1,24 @@
-USE [DEV_ACADEMY]
-GO
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER PROCEDURE [SP_GET_COURSES_BY_STUDENT]
+CREATE OR ALTER PROCEDURE [SP_GET_COURSES_BY_STUDENT]
     @StudentID VARCHAR(10)
 AS
 BEGIN
-    SELECT DISTINCT
-        C.course_id,
-        C.course_code,
-        C.course_name,
-        C.is_active,
-        C.created_at,
-        C.updated_at
-    FROM STUDENT S
-JOIN ATTENDANCE_RECORD A ON S.StudentID = A.StudentID
-JOIN CLASS_SCHEDULE CS ON A.Class_ScheID = CS.Class_ScheID
-JOIN CLASS CL ON CS.ClassID = CL.ClassID
-JOIN COURSE C ON CL.course_id = C.course_id
-    WHERE S.StudentID = @StudentID
+	SELECT DISTINCT
+		CO.course_id AS CourseID,
+		CO.course_name AS CourseName,
+		SU.SubjectID,
+		SU.SubjectName
+	FROM CLASS_ENROLLMENT CE
+	JOIN CLASS CL ON CE.ClassID = CL.ClassID
+	JOIN Course CO ON CO.course_id = CL.course_id
+	JOIN SEMESTER SE ON SE.course_id = CO.course_id
+	JOIN SUBJECT SU ON SE.SemesterID = SU.SemesterID
+	WHERE StudentID = @StudentID
+		AND is_active = 1
 END
 
 
