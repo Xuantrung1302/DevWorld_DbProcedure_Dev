c066af1a83fb7aec9de5702c5cee6bdb5d095a08
Dinh Manh Tri <dinhmanhtri002@gmail.com>
2025-08-04 23:42:38 +0700

    Ghi cong nhan vien

---
+++ SP_INSERT_WORKDAY.sql
@@ -0,0 +1,19 @@
+﻿CREATE OR ALTER PROCEDURE SP_INSERT_WORKDAY
+    @EmployeeID VARCHAR(10),
+    @RecordDate DATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Kiểm tra đã có công hôm nay chưa
+    IF NOT EXISTS (
+        SELECT 1 FROM PAYROLL
+        WHERE EmployeeID = @EmployeeID
+          AND RecordDate = @RecordDate
+          AND WorkDays = 1
+    )
+    BEGIN
+        INSERT INTO PAYROLL (PayrollID, EmployeeID, WorkDays, RecordDate)
+        VALUES (NEWID(), @EmployeeID, 1, @RecordDate);
+    END
+END
