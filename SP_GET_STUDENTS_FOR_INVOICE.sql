41c585859c458f81232c6a72abab861a1154f04a
xuantrung <xuantrung@bee-system.vn>
2025-08-08 01:51:02 +0700

    update

---
+++ SP_GET_STUDENTS_FOR_INVOICE.sql
@@ -0,0 +1,19 @@
+﻿CREATE OR ALTER PROCEDURE [dbo].[SP_GET_STUDENTS_FOR_INVOICE]
+    @StudentID VARCHAR(10) = NULL
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT 
+        s.StudentID, 
+        s.FullName
+    FROM STUDENT s
+    WHERE s.DELETE_FLG = 0 -- nếu có cột xóa mềm
+      AND (
+            @StudentID IS NULL OR 
+            s.StudentID LIKE '%' + @StudentID + '%'
+          )
+    ORDER BY s.StudentID
+END
+
+--exec SP_GET_STUDENTS_FOR_INVOICE 'HV001'
